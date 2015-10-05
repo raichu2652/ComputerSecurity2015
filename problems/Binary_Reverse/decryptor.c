dcfdd95c83b281c6e0b7de2914a06dd3d0b57a95
Paul Tu <raichu2652@gmail.com>
2015-10-05 17:17:44 +0800

    Homework 0x00: Organize Binary Reverse
    comment out print lines in Binary Reverse
    delete irrelevant files

--- problems/Binary_Reverse/decryptor.c
+++ problems/Binary_Reverse/decryptor.c
@@ -17,16 +17,16 @@ void decrypt(int strlen, unsigned char* cipher, unsigned char* text) {
     text[i + 2] = cipher[i] ^ cipher[i + 3];
     text[i + 3] = cipher[i] ^ cipher[i + 1] ^ cipher[i + 2];
 
-    printf("%d: %X <- %X XOR %X\n", i, text[i], cipher[i], ~text[i + 4]&0xff);
-    printf("%d: %X <- %X XOR %X\n", i + 1, text[i + 1], cipher[i + 1], ~text[i + 5]&0xff);
-    printf("%d: %X <- %X XOR %X\n", i + 2, text[i + 2], cipher[i + 2], ~text[i + 6]&0xff);
-    printf("%d: %X <- %X XOR %X\n", i + 3, text[i + 3], cipher[i + 3], ~text[i + 7]&0xff);
+//    printf("%d: %X <- %X XOR %X\n", i, text[i], cipher[i], ~text[i + 4]&0xff);
+//    printf("%d: %X <- %X XOR %X\n", i + 1, text[i + 1], cipher[i + 1], ~text[i + 5]&0xff);
+//    printf("%d: %X <- %X XOR %X\n", i + 2, text[i + 2], cipher[i + 2], ~text[i + 6]&0xff);
+//    printf("%d: %X <- %X XOR %X\n", i + 3, text[i + 3], cipher[i + 3], ~text[i + 7]&0xff);
   }
 }
 
 int main(int argc, char** argv) {
   char* filename = argv[1];
-  printf("filename: %s\n", filename);
+//  printf("filename: %s\n", filename);
 
   FILE* input = fopen(filename, "rt");
   if (input == NULL) {
@@ -48,8 +48,8 @@ int main(int argc, char** argv) {
 
   unsigned char* text = (unsigned char *)malloc(sizeof(unsigned char)*result);
   memcpy(text, cipher, result);
-  printf("%d\n", result);
-  printf("%s\n", text);
+//  printf("%d\n", result);
+//  printf("%s\n", text);
 
   decrypt(result, cipher, text);
   printf("%s\n", text);
